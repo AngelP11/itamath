5800001976c116a2e10b60066ebcd76796e494c0
Angel Ponce <angelgabrielponce@gmail.com>
2017-03-10 12:28:36 -0400

    Ya se colocaron las diferentes rutas de la pagina en en servidor y se agreago la funcionalidad de uso de un motor de template 'pug' y la posibilidad de servir archivos estaticos al servidor

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 var express = require('express')
 var app = express()
 
+app.set('view engine', 'pug') //Utilizamos Pug como templete para las vistas
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function (req, res) {
-	res.send('Hola mundo!!')
+  res.render('index')
+})
+
+app.get('/tips', function (req, res) {
+  res.send('Hola men :D')
+})
+
+app.get('/guias', function (req, res) {
+  res.render('index')
+})
+
+app.get('/reflexiones', function (req, res) {
+  res.render('index')
 })
 
 app.listen(3000, function (err) {
-	if (err) {
-		console.log(err)
-	}
+  if (err) {
+    console.log(err)
+  }
 
-	console.log('Itamar is running in port 3000 :v')
+  console.log('Itamath is running in port 3000 :v')
 })
